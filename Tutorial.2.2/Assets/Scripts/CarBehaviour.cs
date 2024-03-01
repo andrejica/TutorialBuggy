32da90d111ec2e0654ce14cca8aad27bd20c2b84
Aleksandar Andrejic <andrejic.aleks91@gmail.com>
2024-03-01 15:07:17 +0100

    Update next chapter
    feat: wheel friction - Chapter 2.2.6 added to game

--- Tutorial.2.2/Assets/Scripts/CarBehaviour.cs
+++ Tutorial.2.2/Assets/Scripts/CarBehaviour.cs
@@ -6,23 +6,37 @@ public class CarBehaviour : MonoBehaviour
 {
     public WheelCollider wheelColliderFL;
     public WheelCollider wheelColliderFR;
+    public WheelCollider wheelColliderRL;
+    public WheelCollider wheelColliderRR;
     public float maxTorque = 500;
     public float maxSteerAngle = 45;
-    
-    void Start () {}
+    public float sidewaysStiffness = 1.5f;
+    public float forewardStiffness = 1.5f;
+
+    public float maxSpeedKMH = 150;
+    public float maxSpeedBackwardKMH = 30;
+    private float _currentSpeedKMH = 0f;
+
+    void Start()
+    {
+        SetWheelFrictionStiffness(forewardStiffness, sidewaysStiffness);
+    }
     
     void FixedUpdate ()
-    { SetMotorTorque(maxTorque * Input.GetAxis("Vertical"));
+    { 
+        SetMotorTorque(maxTorque * Input.GetAxis("Vertical"));
         SetSteerAngle(maxSteerAngle * Input.GetAxis("Horizontal"));
     }
     
     void SetSteerAngle(float angle)
-    { wheelColliderFL.steerAngle = angle;
+    { 
+        wheelColliderFL.steerAngle = angle;
         wheelColliderFR.steerAngle = angle;
     }
     
     void SetMotorTorque(float amount)
-    { wheelColliderFL.motorTorque = amount;
+    { 
+        wheelColliderFL.motorTorque = amount;
         wheelColliderFR.motorTorque = amount;
     }
 
@@ -30,5 +44,21 @@ public class CarBehaviour : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    void SetWheelFrictionStiffness(float newForwardStiffness, float newSidewaysStiffness)
+    {
+        WheelFrictionCurve fwWFC = wheelColliderFL.forwardFriction;
+        WheelFrictionCurve swWFC = wheelColliderFL.sidewaysFriction;
+        fwWFC.stiffness = newForwardStiffness;
+        swWFC.stiffness = newSidewaysStiffness;
+        wheelColliderFL.forwardFriction = fwWFC;
+        wheelColliderFL.sidewaysFriction = swWFC;
+        wheelColliderFR.forwardFriction = fwWFC;
+        wheelColliderFR.sidewaysFriction = swWFC;
+        wheelColliderRL.forwardFriction = fwWFC;
+        wheelColliderRL.sidewaysFriction = swWFC;
+        wheelColliderRR.forwardFriction = fwWFC;
+        wheelColliderRR.sidewaysFriction = swWFC;
     }
 }
