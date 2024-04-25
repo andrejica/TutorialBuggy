68370e463f501d30c8836bfd1b1851605e2f3c2e
Vent91 <gamer.vent91@gmail.com>
2024-04-25 02:25:11 +0200

    Implement 2.7.3 / 2.7.4
    Add: Save and Load functions
    Add: UI Element hirarchy for canvas elements
    TODO: Implement UI Slider etc. elements for other car attributes

--- Tutorial.2.2/Assets/Scripts/MenuBehaviour.cs
+++ Tutorial.2.2/Assets/Scripts/MenuBehaviour.cs
@@ -1,26 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class MenuBehaviour : MonoBehaviour
 {
+    public WheelCollider wheelColliderFL;
+    public WheelCollider wheelColliderFR;
+    public WheelCollider wheelColliderRL;
+    public WheelCollider wheelColliderRR;
+    public Slider sliSuspDistance;
+    public TMP_Text txtDistanceNum;
+    private Prefs _prefs;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _prefs = new Prefs();
+        _prefs.Load();
+        _prefs.SetAll(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
+        sliSuspDistance.value = _prefs.suspensionDistance;
+        txtDistanceNum.text = sliSuspDistance.value.ToString("0.00");
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void OnSliderChangedSuspDistance(float suspDistance)
     {
-        
+        txtDistanceNum.text = sliSuspDistance.value.ToString("0.00");
+        _prefs.suspensionDistance = sliSuspDistance.value;
+        _prefs.SetWheelColliderSuspension(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
     }
     
     public void OnBtnStartClick()
     {
+        _prefs.Save();
         SceneManager.LoadScene("Scene1");
     }
     
-    
+    void OnApplicationQuit() { _prefs.Save(); }
 }
--- Tutorial.2.2/Assets/Scripts/MenuScene1Behaviour.cs
+++ Tutorial.2.2/Assets/Scripts/MenuScene1Behaviour.cs
@@ -5,10 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuScene1Behaviour : MonoBehaviour
 {
+    public WheelCollider wheelColliderFL;
+    public WheelCollider wheelColliderFR;
+    public WheelCollider wheelColliderRL;
+    public WheelCollider wheelColliderRR;
+    private Prefs _prefs;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _prefs = new Prefs();
+        _prefs.Load();
+        _prefs.SetAll(ref wheelColliderFL, ref wheelColliderFR, ref wheelColliderRL, ref wheelColliderRR);
     }
 
     // Update is called once per frame
